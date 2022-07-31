a2d3bb6b61eee29eb9aa09ca71d4e064669eae66
Kevin Soto <kevinalexandersoto999@gmail.com>
2022-07-31 16:20:11 -0500

    Agregar archivos de proyecto.

---
+++ AppClase/Form1.cs
@@ -0,0 +1,20 @@
+namespace AppClase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
